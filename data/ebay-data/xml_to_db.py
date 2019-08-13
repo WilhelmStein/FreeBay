0be17aaaef3080fe7e7066f9cae742ffd184e473
billsioros <billsioros97@gmail.com>
2019-08-13 19:33:24 +0300

    Added resizing back

--- data/ebay-data/xml_to_db.py
+++ data/ebay-data/xml_to_db.py
@@ -16,7 +16,7 @@ if example_id in parser.auctions:
 
     print(parser.dumps(example_id), sep='\n')
 
-generator = Generator(normalized_image_size=None)
+generator = Generator()
 
 for auction in parser.auctions.values():
 
