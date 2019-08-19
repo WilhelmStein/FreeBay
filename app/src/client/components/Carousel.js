0806df1004d17a5279130ef2bbe8530067327b6a
WilhelmStein <johnfreeman16561@gmail.com>
2019-08-19 19:14:12 +0300

    preliminary styling for carousel

--- app/src/client/components/Carousel.js
+++ app/src/client/components/Carousel.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Carousel, CarouselSlide } from 'material-ui-carousel';
-import { SquareAuctionItem } from '../components/SearchResults';
+import { Card, CardContent, CardMedia, Typography, Box, Grid, Button } from '@material-ui/core';
+import Rating from '@material-ui/lab/Rating';
 import axios from 'axios';
 
 import '../style/Carousel.scss';
@@ -38,7 +39,7 @@ export default class CarouselWrapper extends Component {
                     {this.state.auctions.map((item) => {
                         return  <CarouselSlide key={item.Id}>
                                     <div className = "FeaturedItemWrapper Square">
-                                        <SquareAuctionItem item={item}/>
+                                        <FeaturedItem item={item}/>
                                     </div>
                                 </CarouselSlide>
                     })}
@@ -46,4 +47,75 @@ export default class CarouselWrapper extends Component {
             </div>
         )
     }
+}
+
+function FeaturedItem(props)
+{
+    const rating = Math.round((props.item.User.Seller_Rating * 5.0) / 100.0 * 2) / 2;
+
+    return (
+        <Card className="Item">
+            <CardMedia
+                image={`/api/image?path=${props.item.Images[0].Path}`}
+                title={props.item.Name}
+            />
+            <CardContent className="ItemBody">
+                <Typography variant="h2">
+                    {props.item.Name}
+                </Typography>
+
+                <Box mb={3}>
+                    <Typography display="inline"> Sold By:</Typography>
+
+                    <Typography className="Seller" display="inline" variant="h5">
+                        &nbsp; &nbsp;{props.item.User.Username}
+                    </Typography>
+                        
+                    <Rating display="inline" value={rating} precision={0.5} readOnly />
+                </Box>
+                
+                <Grid container className="Prices" spacing={1}>
+                    <Grid item xs={6}>
+                        <Typography variant="h5" className="Title">Starting Price:</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography className="Starting Price" variant="h4">EUR {props.item.First_Bid.toFixed(2)}</Typography>
+                    </Grid>
+
+                    <Grid item xs={6}>
+                        <Typography variant="h5" className="Title">Current Price:</Typography>
+                    </Grid>
+                    <Grid item xs={6} zeroMinWidth>
+                        <Typography className="Current Price" variant="h4">EUR {props.item.Currently.toFixed(2)}</Typography>
+                    </Grid>
+
+                    <Grid item xs={6}>
+                        <Typography variant="h5" className="Title">Buyout Price:</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography className="Buyout Price" variant="h4">EUR {props.item.Buy_Price.toFixed(2)}</Typography>
+                    </Grid>
+                </Grid>
+
+                <Grid container className="Timer">
+                    <Grid item xs={6}>
+                        <Typography className="Title" variant="h4">Time Remaining:</Typography>
+                    </Grid>
+
+                    <Grid item xs={6}>
+                        <Typography className="Time_Remaining" variant="h3">{(new Date(props.item.Ends) - new Date(props.item.Started))/1000/60/60/24}</Typography>
+                    </Grid>
+                </Grid>
+
+                <Box mt={3} className="Buttons">
+                    <Button className="Bid Button" variant="contained">
+                        Bid
+                    </Button>
+                    <Button className="Buyout Button" variant="contained">
+                        Buyout
+                    </Button>
+                </Box>
+            </CardContent>
+        </Card>
+    )
 }
