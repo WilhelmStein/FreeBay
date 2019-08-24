e57c9ea8c1a542a405081900b348ee35450f15f1
billsioros <billsioros97@gmail.com>
2019-08-24 22:17:08 +0300

    Auction Page is more presentable :P

--- app/src/client/components/Auction.js
+++ app/src/client/components/Auction.js
@@ -3,6 +3,13 @@ import autoBind from 'auto-bind';
 import { withRouter } from 'react-router-dom';
 import axios from "axios";
 
+import { List, ListItem, ListItemText, IconButton } from '@material-ui/core';
+import { GridList, GridListTile } from '@material-ui/core'
+import { Avatar } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
+import { Card, CardHeader, CardMedia, CardContent, CardActions } from '@material-ui/core';
+import { ExpandMore } from '@material-ui/icons'
+import { Typography, Collapse } from '@material-ui/core'
 
 class AuctionPage extends Component
 {
@@ -12,6 +19,7 @@ class AuctionPage extends Component
 
         this.state = {
             id: props.match.params.id,
+            expanded: false,
             auction: null
         };
 
@@ -44,27 +52,116 @@ class AuctionPage extends Component
             return null;
 
         return (
-            <div className="auction">
-                <h2 className="auction-title">{this.state.auction.Name}</h2>
-                <div className="auction-images">
-                    {(this.state.auction.Images.length != 0
+            <Paper>
+                <Card className="auction-page">
+                    <CardHeader
+                        avatar={
+                            <Avatar
+                                aria-label="auction-avatar"
+                                className="auction-avatar"
+                                src={this.state.auction.Images.length !== 0 ? `/api/image?path=${this.state.auction.Images[0]}` : "https://dummyimage.com/250x250/ffffff/4a4a4a.png&text=No+Image"}
+                            >
+                            </Avatar>
+                        }
+                        title={this.state.auction.Name}
+                        subheader={`${this.state.auction.User.Username} - ${this.state.auction.User.Rating}`}
+                    >
+                    </CardHeader>
+                    <CardMedia
+                        className="auction-first-image"
+                        image={this.state.auction.Images.length !== 0 ? `/api/image?path=${this.state.auction.Images[0]}` : "https://dummyimage.com/250x250/ffffff/4a4a4a.png&text=No+Image"}
+                        title={this.state.auction.Name}
+                    />
+                    <GridList
+                        className="auction-images"
+                        cellHeight={160}
+                        cols={3}
+                    >
+                        {this.state.auction.Images.map(image =>
+                            <GridListTile key={image.Path}>
+                                <img src={image.Path}/>
+                            </GridListTile>
+                        )}
+                    </GridList>
+                    <CardContent className="auction-money">
+                        <Typography variant="body1" color="textPrimary" component="p">
+                            {this.state.auction.Buy_Price != null ? "Buy Price: " + this.state.auction.Buy_Price : null}
+                            First Bid: {this.state.auction.First_Bid}
+                            Currently: {this.state.auction.Currently}
+                        </Typography>
+                    </CardContent>
+                    <CardContent className="auction-location">
+                        <Typography variant="body1" color="textPrimary" component="p">
+                            {this.state.auction.Longitude !== null && this.state.auction.Latitude !== null
+                            ?
+                            "Longitude: " + this.state.auction.Longitude +
+                            "Latitude: " + this.state.auction.Latitude
+                            :
+                            null}
+                            Location: {this.state.auction.Location}
+                        </Typography>
+                    </CardContent>
+                    <CardContent className="auction-duration">
+                        <Typography variant="body1" color="textPrimary" component="p">
+                            Started: {this.state.auction.Started}
+                            Ends: {this.state.auction.Ends}
+                        </Typography>
+                    </CardContent>
+                    <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            {this.state.auction.Description}
+                        </Typography>
+                    </Collapse>
+                    <CardActions disableSpacing>
+                        <IconButton className='auction-description-expand'
+                            onClick={() => { this.setState({expanded: !this.state.expanded})}}
+                            aria-expanded={this.state.expanded}
+                            aria-label={"Show More"}
+                        >
+                            <ExpandMore/>
+                        </IconButton>
+                    </CardActions>
+                    <CardContent className="auction-bids">
+                    {(this.state.auction.Bids.length !== 0
                     ?
-                    this.state.auction.Images.map(image => this.image(`/api/image?path=${image.Path}`))
+                    (
+                        <List component="div">
+                            {this.state.auction.Bids.map(bid =>
+                                <Bid
+                                    Id={bid.Id}
+                                    Username={bid.User.Username}
+                                    Rating={bid.User.Rating}
+                                    Amount={bid.Amount}
+                                    Time={bid.Time}
+                                >
+                                </Bid>
+                            )}
+                        </List>
+                    )
                     :
-                    this.image("https://dummyimage.com/250x250/ffffff/4a4a4a.png&text=No+Image"))
-                    }
-                </div>
-                <div className="auction-location">
-                    <h3>{this.state.auction.Location}</h3>
-                    <h4>{this.state.auction.Latitude}</h4>
-                    <h4>{this.state.auction.Longitude}</h4>
-                </div>
-                <div className="auction-duration">
-                    <h3>{this.state.auction.Started} - {this.state.auction.Ended}</h3>
-                </div>
-            </div>
+                    null
+                    )}
+                    </CardContent>
+                </Card>
+            </Paper>
+            //     <div className="auction-description">
+            //         <h4>{this.state.auction.Description}</h4>
+            //     </div>
         );
     }
+}
+
+function Bid(props)
+{
+    return (
+        <ListItem key={props.Id}>
+            <Avatar className="auction-avatar">{props.Username[0]}</Avatar>
+            <ListItemText
+                primary={`${props.Username} ${props.Rating}`}
+                secondary={`Amount: ${props.Amount} Time: ${props.Time}`}>
+            </ListItemText>
+        </ListItem>
+    )
 }
 
 export default withRouter(AuctionPage);
