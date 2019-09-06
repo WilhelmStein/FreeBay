e8426c210bbeefaf442c4179a3f1e22d77914ae2
WilhelmStein <johnfreeman16561@gmail.com>
2019-09-06 14:34:58 +0300

    more styling being done

--- app/src/client/components/About.js
+++ app/src/client/components/About.js
@@ -5,72 +5,65 @@ import { Grid, Card, CardHeader, CardContent, Typography, CardMedia } from '@mat
 import '../style/About.scss';
 import airplane from '../images/airplane.png';
 import transport from '../images/transport.png';
+import map from '../images/map.png';
 
 export default function About() {
 
     let data = [
         {
-            header: 'About Freebay', 
-            text: `Launched in 2019, FreeBay is the leading platform for global wholesale trade.
-                   We serve millions of buyers and suppliers around the world.`,
-            image: airplane
+            header: {text: 'About Freebay', style:{backgroundColor: '$lesser-white'}},
+            content: {
+                text: `Launched in 2019, FreeBay is the leading platform for global wholesale trade.
+                    We serve millions of buyers and suppliers around the world.`,
+                style:{width: '50%', float:'left'}
+            },
+            media: {
+                src: airplane,
+                style: {width: '50%', float: 'right'}
+            }
         },
         {
-            header: 'Our Mission', 
-            text: `Is to make it easy to do business anywhere.
-                   We do this by giving suppliers the tools necessary to reach a global audience for their products,
-                   and by helping buyers find products and suppliers quickly and efficiently.`,
-            image: transport
+            header: {text: 'Our Mission', style:{width: '50%', float:'right'}},
+            content: {
+                text: `Is to make it easy to do business anywhere.
+                    We do this by giving suppliers the tools necessary to reach a global audience for their products,
+                    and by helping buyers find products and suppliers quickly and efficiently.`,
+                style:{width: '50%', float:'right'}
+            },
+            media: {
+                src: transport,
+                style:{width: '50%', float:'left'}
+            }
         },
         {
-            header: 'One-Stop Sourcing', 
-            text: `Freebay brings you hundreds of millions of products in over 40 different major categories, including consumer
-                   electronics, machinery and apparel. Buyers for these products are located in 190+ countries and regions,
-                   and exchange hundreds of thousands of messages with suppliers on the platform each day.`,
-            image: 'https://bit.ly/2m0GOXI'
+            header: {text: 'One-Stop Sourcing', style:{backgroundColor: '$lesser-white'}},
+            content: {
+                text: `Freebay brings you hundreds of millions of products in over 40 different major categories, including consumer
+                    electronics, machinery and apparel. Buyers for these products are located in 190+ countries and regions,
+                    and exchange hundreds of thousands of messages with suppliers on the platform each day.`,
+                style:{width: '50%', float:'left'}
+            },
+            media: {
+                src: map,
+                style: {width: '50%', float: 'right'}
+            }
         }
     ];
 
     let content = data.map((item, index) => {
+        return <Card className={`AboutCard ${(index % 2 == 0) ? ('Even') : ('Odd')}`}>
+                    <CardHeader title={<Typography className="Title" style={item.header.style}>{item.header.text}</Typography>}/>
 
-        if(index % 2 == 0)
-        {
-            return <Card className={`AboutCard Even`}>
-                        <CardHeader title={<Typography className="Title">{item.header}</Typography>}/>
-
-                        <Grid container>
-                            <Grid item>
-                                <CardContent className="Text">
-                                    {item.text}
-                                </CardContent>
-                            </Grid>
-                            
-                            <Grid item>
-                                <CardMedia className="Media" image={item.image}/>
-                            </Grid>
-                        </Grid>
-                    </Card>
-        }
-        else
-        {
-            return <Card className={`AboutCard Odd`}>
-                        <CardHeader title={<Typography className="Title">{item.header}</Typography>}/>
-
-                        <Grid container>
-
-                            <Grid item xs={6}>
-                                <CardMedia className="Media" image={item.image}/>
-                            </Grid>
-
-                            <Grid item xs={6}>
-                                <CardContent className="Text">
-                                    {item.text}
-                                </CardContent>
-                            </Grid>
-                        </Grid>
-                    </Card>
-        }
-
+                        <div style={item.content.style}>
+                            <CardContent className="Text">
+                                {item.content.text}
+                            </CardContent>
+                        </div>
+                        
+                        <div style={item.media.style}>
+                            <CardMedia className="Media" image={item.media.src}/>
+                        </div>
+                </Card>
     }) 
 
     return(
